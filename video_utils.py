c41de74119c2e3768a10d88a09cb9d2b099be8e2
seryibizkit <seryibizkit@gmail.com>
2024-11-29 15:49:34 +0300

    thumbnails util

--- video_utils.py
+++ video_utils.py
@@ -10,3 +10,35 @@ def video_list(video_path = ""):
             filename = os.path.join(video_path, file)
             videos_list.__setitem__(file,filename)
     return videos_list
+
+def file_thumbnail(source_path):
+    # Загрузка видеофайла
+    name = os.path.splitext(os.path.basename(source_path))[0]
+
+    # Создание папки для thumbnail
+    thumbnails_dir = "static/thumbnails"
+    os.makedirs(thumbnails_dir, exist_ok=True)
+    new_image_filepath = os.path.join(thumbnails_dir, f"{name}_thumbnail.jpg")
+    if os.path.isfile(new_image_filepath ):
+        return new_image_filepath
+    else:
+        # Создание объекта VideoFileClip
+        clip = VideoFileClip(source_path)
+
+        # Получение количества кадров в секунду
+        fps = clip.reader.fps
+
+        # Получение количества кадров в видео
+        nframes = clip.reader.n_frames
+
+        # Получение длительности видео в секундах
+        duration = clip.duration
+
+        # Создание thumbnail на определенном кадре
+        frame_at_second = float(duration * 10 / 100)
+        frame = clip.get_frame(frame_at_second)
+
+        # Сохранение thumbnail в файл
+        new_image = Image.fromarray(frame)
+        new_image.save(new_image_filepath)
+        return new_image_filepath
